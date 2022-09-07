c00c2a7360d7c02b4bd1d83616c954830d77bea0
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-09-07 16:42:11 +0000

    Added function to print hangman when answering wrong

--- run.py
+++ run.py
@@ -29,3 +29,48 @@ randomWord = random.choice(wordDictionary)
 
 for i in randomWord:
     print("_", end=" ")
+
+
+def print_hangman(wrong):
+    if (wrong == 0):
+        print("\n+---+")
+        print("    |---+")
+        print("    |---+")
+        print("    |---+")
+        print("   ===")
+    elif (wrong == 1):
+        print("\n+---+")
+        print("O   |---+")
+        print("    |---+")
+        print("    |---+")
+        print("   ===")
+    elif (wrong == 2):
+        print("\n+---+")
+        print("O   |---+")
+        print("|   |---+")
+        print("    |---+")
+        print("   ===")
+    elif (wrong == 3):
+        print("\n+---+")
+        print(" O   |---+")
+        print("/|   |---+")
+        print("     |---+")
+        print("    ===")
+    elif (wrong == 4):
+        print("\n+---+")
+        print(" O   |---+")
+        print("/|\  |---+")
+        print("     |---+")
+        print("    ===")
+    elif (wrong == 5):
+        print("\n+---+")
+        print(" O   |---+")
+        print("/|\  |---+")
+        print("/    |---+")
+        print("    ===")
+    elif (wrong == 6):
+        print("\n+---+")
+        print(" O__||---+")
+        print("/|\  |---+")
+        print("/ \  |---+")
+        print("    ===")
